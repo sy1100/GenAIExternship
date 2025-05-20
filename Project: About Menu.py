6a6645695fadc2da83f0ff79df004794c7208e14
sy1100 <sierrayang12@gmail.com>
2025-05-20 00:06:19 -0500

    Project_5

---
+++ Project: About Menu.py
@@ -0,0 +1,77 @@
+import turtle
+
+def factorial(num):
+    if num == 1:
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+def fibonacci(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibonacci(num - 1) + fibonacci(num - 2)
+
+def tree_setup():
+    global t
+    t = turtle.Turtle()
+    t.reset()
+    t.speed(6)
+    t.color("black")
+    t.left(90)
+    t.up()
+    t.backward(100)
+    t.down()
+
+def fractal(length, depth=0):
+    if depth <4:
+        depth += 1
+        next_length = length *(0.7 ** depth)
+        t.forward(length)
+        t.left(60)
+        fractal(next_length, depth)
+        t.right(30)
+        fractal(next_length, depth)
+        t.right(30)
+        fractal(next_length, depth)
+        t.right(30)
+        fractal(next_length, depth)
+        t.right(30)
+        fractal(next_length, depth)
+        t.left(60)
+        t.backward(length)
+
+def main():
+    print("Welcome to the Recursive Artistry Program"
+          "\nChoose an option:"
+          "\n1. Calculate Factorial"
+          "\n2. Find Fibonacci"
+          "\n3. Draw a Recursive Fractal"
+          "\n4. Exit")
+    user_input = int(input("> "))
+
+    if user_input == 1:
+        num = int(input("Enter a number to find its factorial: "))
+        print("\nFactorial of " + str(num) + " is", factorial(num),"\n")
+        return main()
+
+    elif user_input == 2:
+        num = int(input("Enter a number to find its Fibonacci: "))
+        print( "The " + str(num) +"th Fibonacci number is", fibonacci(num),"\n")
+        return main()
+
+
+    elif user_input == 3:
+        print("A simple fractal tree drawing! 🌳\n")
+        tree_setup()
+        fractal(100)
+        turtle.done()
+        return main()
+
+    elif user_input == 4:
+        exit()
+
+main()
+
